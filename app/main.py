c404116e961ef5700b222e809131e304268a363c
Willamy Silva <willamycurso@gmail.com>
2025-12-03 12:30:18 -0300

    Refactor database connection to use pyodbc

--- app/main.py
+++ app/main.py
@@ -1,35 +1,20 @@
+import pyodbc
 from fastapi import FastAPI
-import pymssql
 
 app = FastAPI()
 
-AZURE_SERVER = "pbdb3073.database.windows.net"
-AZURE_DATABASE = "PBDB3073"
-AZURE_USER = "admrs"
-AZURE_PASSWORD = "Gf3$Rn8!Qb12^KsW0tZ"
+def get_connection():
+    conn = pyodbc.connect(
+        "DRIVER={ODBC Driver 18 for SQL Server};"
+        "SERVER=pbdb3073.database.windows.net;"
+        "DATABASE=PBDB3073;"
+        "UID=admrs;"
+        "PWD=Gf3$Rn8!Qb12^KsW0tZ;"
+        "Encrypt=yes;"
+        "TrustServerCertificate=no;"
+    )
+    return conn
 
 @app.get("/")
-def home():
-    return {"status": "API rodando!"}
-
-@app.get("/visitas")
-def get_visitas():
-    try:
-        conn = pymssql.connect(
-            server=AZURE_SERVER,
-            user=AZURE_USER,
-            password=AZURE_PASSWORD,
-            database=AZURE_DATABASE
-        )
-
-        cursor = conn.cursor(as_dict=True)
-
-        cursor.execute("SELECT * FROM TAB_REGISTRO_VISITA_SUPERVISAO_CABECALHO")
-        dados = cursor.fetchall()
-
-        conn.close()
-
-        return dados
-
-    except Exception as e:
-        return {"erro": str(e)}
+def root():
+    return {"status": "ok"}
